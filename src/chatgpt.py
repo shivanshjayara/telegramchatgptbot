b36f99118d707a0f00b9f793e1ff260f90a041a7
samyakai <Samyak.aitools@gmail.com>
2023-05-01 21:48:09 +0530

    updating chatgpt.py file

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -30,20 +30,28 @@ class Reference:
     """
     A class to store the previous response from the chatGPT API.
     """
-    raise NotImplementedError
-
+    # raise NotImplementedError
+    def __init__(self) ->None:
+        self.response=""
 
 # Load environment variables
 load_dotenv()
 
 # Set up OpenAI API key
+openai.api_key=os.getenv("OPENAI_API_KEY")
 
 # Create a reference object to store the previous response
+reference=Reference()
+
+
 
 # Bot token can be obtained via https://t.me/BotFahter
 TOKEN = os.getenv("TOKEN")
 
 # Model used in chatGPT
+MODEL_NAME="gpt-3.5-turbo"
+
+
 
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
@@ -53,35 +61,74 @@ def clear_past():
     """
     A function to clear the previous conversation and context.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    reference.response=""
+
+
+
+
+
+
 
 @dispatcher.message_handler(commands=['start'])
 async def welcome(message: types.Message):
     """
-    A handler to welcome the user and clear past conversation and context.
+    A handler to welcome the user and 
+    clear past conversation and context.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    clear_past()
+    await message.reply(f"hello!\nI am chat gpt bot created\
+                         by shivansh!\nHow may I assist you today")
 
 @dispatcher.message_handler(commands=['clear'])
 async def clear(message: types.Message):
     """
     A handler to clear the previous conversation and context.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    clear_past()
+    await message.reply(f"I have cleared the past\
+                         conversation and context")
+
 
 @dispatcher.message_handler(commands=['help'])
 async def helper(message: types.Message):
     """
     A handler to display the help menu.
     """
-    raise NotImplementedError
+    help_command="""
+    Hi there, I'm chatGpt bot created by shivansh!. Please follow these commands-
+    /start - to strt the conversation    
+    /clear - to clear the past conversation and context
+    /help - to get this help menu.
+    I hope this helps.
+    """
+    # raise NotImplementedError
+    await message.reply(help_command)
+
+
 
 @dispatcher.message_handler()
 async def chatgpt(message: types.Message):
     """
     A handler to process the user's input and generate a response using the chatGPT API.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    
+    print(f">>> USER:\n\t{message.text}")
+    response=openai.ChatCompletion.create(
+        model=MODEL_NAME,
+        messages=[
+            {"role":"assistant","content": reference.response},
+            {"role":"user", "content":message.text}
+        ]
+    )
+    reference.response=response['choices'][0]['message']['content']
+
+    print(f">>> chatGpt:\n\t{reference.response}")
+    await bot.send_message(chat_id=message.chat.id,
+                           text=reference.response)
 
 
 if __name__ == '__main__':
